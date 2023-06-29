09da3bbb210a2a1802f77ab78246ad99b1ac8510
venutti <lvenutti@fi.uba.ar>
2023-06-29 12:40:33 -0300

    Add .env usage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,8 @@ import Logo from "../resources/pokemon-logo.jpg";
 export default function Header() {
   return (
     <header className="headerContent">
+      <p>This application is running in: {process.env.NODE_ENV}</p>
+      <p>{process.env.REACT_APP_MESSAGE}</p>
       <img className="logo" src={Logo} alt="Logo de pokemon" />
     </header>
   );
